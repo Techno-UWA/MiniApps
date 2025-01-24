5a2027b5b7c12a4f4d57f2171d06ba15197c5e7e
John Kosonlawat <techno-uwa@outlook.com>
2025-01-24 10:49:38 +0800

    It worked! Credit to  O1 preview from Github Copilot
    Added HomeContent back to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="bg-white">
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <h1>Welcome to my website</h1>
+          <HomeContent />
         </div>
       </div>
     </div>
